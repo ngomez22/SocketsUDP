771ebf330c58c81783f5f2fb8ac28070ca1194f2
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-03-29 12:01:42 -0500

    Add method to convert Message into Byte array

--- client/src/client/Client.java
+++ client/src/client/Client.java
@@ -1,43 +1,62 @@
 package client;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class Client {
-	
+
 	public static void main(String args[]) throws Exception {
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(
-				System.in));
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		DatagramSocket clientSocket = new DatagramSocket();
 		InetAddress IPAddress = InetAddress.getByName("localhost");
 		byte[] sendData = new byte[1024];
 		byte[] receiveData = new byte[1024];
 		String sentence = inFromUser.readLine();
 		sendData = sentence.getBytes();
-		int i =0;
-		while(i<10)
-		{
-			DatagramPacket sendPacket = new DatagramPacket(sendData,
-					sendData.length, IPAddress, 9876);
+		int i = 0;
+		while (i < 10) {
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
 			clientSocket.send(sendPacket);
-			DatagramPacket receivePacket = new DatagramPacket(receiveData,
-					receiveData.length);
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 			clientSocket.receive(receivePacket);
 			String modifiedSentence = new String(receivePacket.getData());
 			System.out.println("FROM SERVER:" + modifiedSentence);
 			i++;
-		}			
+		}
 		clientSocket.close();
 	}
-	
+
+	public byte[] messageToBytes(Message m) {
+		byte[] message = null;
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		ObjectOutput out = null;
+		try {
+			out = new ObjectOutputStream(bos);
+			out.writeObject(m);
+			out.flush();
+			message = bos.toByteArray();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				bos.close();
+			} catch (IOException ex) {}
+		}
+		return message;
+	}
+
 	public class Message implements Serializable {
 		private int seqNun;
 		private long timestamp;
-		
+
 		public Message(int seqNum) {
 			this.seqNun = seqNum;
 			this.timestamp = System.currentTimeMillis();
@@ -49,6 +68,6 @@ public class Client {
 
 		public long getTimestamp() {
 			return timestamp;
-		}		
+		}
 	}
 }
